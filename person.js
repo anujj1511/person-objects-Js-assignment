ed362c362bfb3d374c30d21d72d69c613aa4d5cc
anujj1511 <90145637+anujj1511@users.noreply.github.com>
2021-11-26 20:59:58 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,21 @@
+var person1 = {
+  FirstName: "Anuj",
+  LastName: "Jadhav",
+  OfficeAdress: "Pune, Maharashtra 412101",
+};
+
+var person2 = {
+  FirstName: "Rahul",
+  LastName: "Kumar",
+  OfficeAdress: "Patna, Bihar 675454",
+};
+
+var person3 = {
+  FirstName: "Ajay",
+  LastName: "Nimbalkar",
+  OfficeAdress: "Mumbai, Maharashtra 565767",
+};
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
